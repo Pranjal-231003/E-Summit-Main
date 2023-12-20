22bbfba789c60ea6599d5a5f651e7f1645f12af3
Jay Aggarwal <jayaggarwal99288@gmail.com>
2023-12-20 19:36:38 +0530

    logo

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./page.css"
+import logo from "./logo.png"
 function LandingPage() {
   return (
     <>
         <div className='Page_container'>
+            <img src={logo} alt="" srcset="" />
             <h1>E-SUMMIT</h1>
             <div className='menu_option'>
                 <a href='#'>FIGURES</a>
