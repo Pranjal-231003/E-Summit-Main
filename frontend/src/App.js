e419f9409cb80c7ff6bf39f5d50cdd22b7e7e080
Ayush Bansal <bansalayush24704@gmail.com>
2023-12-22 00:18:36 +0530

    Preloader done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
+import React, { useState, useEffect } from "react";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Preloader from "./components/Preloader/Preloader";
 
-
 function App() {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoader(false);
+    }, 7000);
+    
+    // Clear the timeout to avoid memory leaks
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-   
-    <LandingPage></LandingPage>
+      {loader ? <Preloader /> : <LandingPage />}
     </>
   );
 }
