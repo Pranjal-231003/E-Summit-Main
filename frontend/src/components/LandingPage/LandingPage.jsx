a3aa07642636cee0dc83085c961a990d3ba329de
Pranjal-2310 <pranjalshp2310@gmail.com>
2023-12-21 14:51:12 +0530

    architecture change

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import "./page.css"
-import logo from "./logo.png"
+import logo from "../images/logo.png"
 function LandingPage() {
   return (
     <>
         <div className='Page_container'>
             <img src={logo} alt="" srcset="" />
             <h1>E-SUMMIT</h1>
-            <div className='menu_option'>
-                <a href='#'>FIGURES</a>
-                <a href='#'>EVENTS</a>
-                <a href='#'>ABOUT</a>
-            </div>
+            {/* <div className='menu_option'> */}
+                {/* <a href='#'>FIGURES</a> */}
+                {/* <a href='#'>EVENTS</a> */}
+                {/* <a href='#'>ABOUT</a> */}
+            {/* </div> */}
         </div>
     </>
   )
---
+++ frontend/src/components/Preloader/Preloader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Preloader.css"
+// import logo from "../images/logo.png"
+function Preloader() {
+  return (
+    <>
+        <div className="page">
+        <div class="e-card playing">
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Preloader
