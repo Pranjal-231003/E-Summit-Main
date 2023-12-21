2f452317aaa76ef6f0491f239e33858ad13b0b33
Ayush Bansal <116891199+Bansalayush247@users.noreply.github.com>
2023-12-22 00:28:02 +0530

    preloader

--- frontend/src/components/Preloader/Preloader.jsx
+++ frontend/src/components/Preloader/Preloader.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import "./Preloader.css"
-// import logo from "../images/logo.png"
+// Preloader.jsx
+
+import React, { useEffect, useRef } from 'react';
+import "./Preloader.css";
+import preloaderVideo from "./preloader.mp4";
+
 function Preloader() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const bounceTimeout = setTimeout(() => {
+      video.style.animation = 'bounceAnimation 2s ease-in-out infinite'; // Start bouncing after 5 seconds
+    }, 5000);
+
+    return () => clearTimeout(bounceTimeout);
+  }, []);
+
   return (
-    <>
-        <div className="page">
-        <div class="e-card playing">
-                <div class="wave"></div>
-                <div class="wave"></div>
-                <div class="wave"></div>
-            </div>
-        </div>
-    </>
-  )
+    <div className="page">
+      <video ref={videoRef} width="100%" height="100%" autoPlay  muted>
+        <source src={preloaderVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
 
-export default Preloader
+export default Preloader;
