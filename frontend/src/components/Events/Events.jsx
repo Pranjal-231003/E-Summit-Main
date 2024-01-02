16480cfdb5fa9fd4b9b8535756e15d0bf980f9ac
Ayush Bansal <bansalayush24704@gmail.com>
2024-01-02 23:37:10 +0530

    Event responsive

--- frontend/src/components/Events/Events.jsx
+++ frontend/src/components/Events/Events.jsx
@@ -41,6 +41,19 @@ function Events() {
         description1: "TIME-6PM",
         description2: "VENUE-OAT",
       }
+      ,
+      {
+        imageUrl: ash,
+        title: "ASHNEER GROVER TALK",
+        description1: "TIME-6PM",
+        description2: "VENUE-OAT",
+      },
+      {
+        imageUrl: ash,
+        title: "ASHNEER GROVER TALK",
+        description1: "TIME-6PM",
+        description2: "VENUE-OAT",
+      }
   ];
 
   return (
@@ -48,6 +61,8 @@ function Events() {
     <div id="Events_container">
     <div id='Events'>
         <h1 className='evh'><span className="text_s">EVENTS</span></h1>
+        <div className="events-container"> 
+
         <div className="events-grid">
       {eventsData.map((event, index) => (
         <Card
@@ -58,51 +73,7 @@ function Events() {
           description2={event.description2}
         />
       ))}
-      {eventsData.map((event, index) => (
-        <Card
-          key={index}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          description1={event.description1}
-          description2={event.description2}
-        />
-      ))}
-      {eventsData.map((event, index) => (
-        <Card
-          key={index}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          description1={event.description1}
-          description2={event.description2}
-        />
-      ))}
-      {eventsData.map((event, index) => (
-        <Card
-          key={index}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          description1={event.description1}
-          description2={event.description2}
-        />
-      ))}
-      {eventsData.map((event, index) => (
-        <Card
-          key={index}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          description1={event.description1}
-          description2={event.description2}
-        />
-      ))}
-      {eventsData.map((event, index) => (
-        <Card
-          key={index}
-          imageUrl={event.imageUrl}
-          title={event.title}
-          description1={event.description1}
-          description2={event.description2}
-        />
-      ))}
+  </div>
     </div>
     </div>
     </div>
