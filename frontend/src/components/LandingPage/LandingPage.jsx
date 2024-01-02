c8162da6e575b9132f9366558e4fc321f6003559
Pranjal-2310 <pranjalshp2310@gmail.com>
2024-01-02 14:37:56 +0530

    text change in about us

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,26 +1,43 @@
 // LandingPage.jsx
-import React from 'react';
+import React from "react";
 import "./page.css";
 import logo from "../images/logo.png";
-import navbar from "../images/navbar.png"
-import withCursor from '../Cursor/Cursor'; // Import the HOC
-
+import navbar from "../images/navbar.png";
+import withCursor from "../Cursor/Cursor"; // Import the HOC
+<script src="//www.tickcounter.com/static/js/loader.js"></script>;
 class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <div id='Page_container'>
-          <div className='rectangle'></div>
-         
-          <nav className='navbar'>
-            <img src={logo} className='logo' alt="" />
-            <img src={navbar} className='onclick' alt="" srcset="" />
-          </nav>
-          <div className="logo_container">
+        <div id="Page_container">
+          <div className="rectangle"></div>
 
+          <nav className="navbar">
+            <img src={logo} className="logo" alt="" />
+            <img src={navbar} className="onclick" alt="" srcset="" />
+          </nav>
+          <div className="logo_container"></div>
+          <h1 className="text-element">
+            <span className="text_s">E-SUMMIT</span>
+          </h1>
+          <div className="design_container">
+            <a
+              data-type="countdown"
+              data-id="4741074"
+              className="tickcounter"
+              style={{
+                display: "block",
+                left: "0",
+                width: "57%",
+                height: "0",
+                position: "relative",
+                paddingBottom: "6%",
+                margin: "0 auto",
+              }}
+              title="E-Summit"
+              href="//www.tickcounter.com/"
+            ></a>
           </div>
-          <h1 className='text-element'><span className='text_s'>E-SUMMIT</span></h1>
-          <div className="design_container"></div>
           {/* Add other text elements as needed */}
           <div className="cursor"></div>
         </div>
