7aec259103657173b18805a0444c19cc201eeb18
Pranjal-2310 <pranjalshp2310@gmail.com>
2024-01-03 23:59:56 +0530

    cards changed in compi

--- frontend/src/components/Competitions/Competitions.jsx
+++ frontend/src/components/Competitions/Competitions.jsx
@@ -1,10 +1,10 @@
 // Rocket.js
 
-import React from 'react';
-import './Competitions.css';
-import Spline from '@splinetool/react-spline';
+import React from "react";
+import "./Competitions.css";
+import Spline from "@splinetool/react-spline";
 
-import Card from './Card/card';
+import Card from "./Card/card";
 import expo from "../images/STARTUP_ EXPO1.png";
 import shark from "../images/shark tank.png";
 import ideathon from "../images/Post3.png";
@@ -14,58 +14,89 @@ const Competitions = () => {
   const competitionsData = [
     {
       imageUrl: expo,
-      title: "ASHNEER GROVER TALK",
-      description:"DESCRIPTION",
-      time: "TIME-6PM",
-      venue: "VENUE-OAT",
+      title: "Start-Up Expo",
+      description:
+        "LNMIIT Startup Expo: Igniting sparks of ideas, propelling innovations to greater heights!",
+      time: "TIME-TBD",
+      venue: "VENUE-TBD",
+      register:
+        "https://unstop.com/conferences/startup-expo-e-summit-lnm-institute-of-information-technology-lnmiit-jaipur-849058",
     },
     {
       imageUrl: shark,
-      title: "ASHNEER GROVER TALK",
-      description:"DESCRIPTION",
-      time: "TIME-6PM",
-      venue: "VENUE-OAT",
+      title: "Shark Tank",
+      description:
+        "Dive into innovation with Shark Tank  at E-Summit, where ideas surface and pitches make a splash!",
+      time: "TIME-TBD",
+      venue: "VENUE-TBD",
+      register:
+        "https://unstop.com/competitions/shark-tank-e-summit-lnm-institute-of-information-technology-lnmiit-jaipur-849015",
     },
     {
       imageUrl: ideathon,
-      title: "ASHNEER GROVER TALK",
-      description:"DESCRIPTION",
-      time: "TIME-6PM",
-      venue: "VENUE-OAT",
+      title: "Ideathon",
+      description:
+        "Ideas ignite, innovation excites: Fueling the future at E-Summit LNMIIT's Kickstart Ideathon",
+      time: "TIME-TBD",
+      venue: "VENUE-TBD",
+      register:
+        "https://unstop.com/competitions/ideathon-e-summit-lnm-institute-of-information-technology-lnmiit-jaipur-847782",
     },
     {
       imageUrl: wolf,
-      title: "ASHNEER GROVER TALK",
-      description:"DESCRIPTION",
-      time: "TIME-6PM",
-      venue: "VENUE-OAT",
+      title: "The Wolf of Dalal Street",
+      description:
+        "Master the Wall Street Bull: Dive into stock market investing with savvy decision-making!",
+      time: "TIME-TBD",
+      venue: "VENUE-TBD",
+      register: "",
     },
     {
       imageUrl: quiz,
-      title: "ASHNEER GROVER TALK",
-      description:"DESCRIPTION",
-      time: "TIME-6PM",
-      venue: "VENUE-OAT",
-    }
+      title: "Biz Quiz",
+      description:
+        "Exciting Quiz testing your entrepreneurial brilliance and business intelligence",
+      time: "TIME-TBD",
+      venue: "VENUE-TBD",
+      register: "",
+    },
   ];
-  const competitionText = window.innerWidth < 640 ? 'COMPETITIONS' : window.innerWidth > 745 ? 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS' : 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS';
+  let competitionText = "";
+  if (window.innerWidth !== null) {
+    competitionText =
+      window.innerWidth < 640
+        ? "COMPETITIONS"
+        : window.innerWidth > 745
+        ? "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS"
+        : "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS";
+  }
 
   return (
-    <div className='main'>
-     <div className='splinemodel'>
-      <div className='overlay-text1'>
-      <h1>{ window.innerWidth > 745 ? 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS' : 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS'}</h1>
+    <div className="main">
+      <div className="splinemodel">
+        <div className="overlay-text1">
+          <h1>
+            {window.innerWidth > 745
+              ? "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS"
+              : "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS"}
+          </h1>
+        </div>
+        <div className="overlay-text2">
+          <h1>{competitionText}</h1>
+        </div>
+        <div className="overlay-text3">
+          <h1>
+            {window.innerWidth > 745
+              ? "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS"
+              : "COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS"}
+          </h1>
+        </div>
+        <Spline
+          className="splines"
+          scene="https://prod.spline.design/5d1cMbEhisQ1tPpS/scene.splinecode"
+        />
       </div>
-      <div className='overlay-text2'>
-  <h1>{competitionText}</h1>
-</div>
-      <div className='overlay-text3'>
-      <h1>{window.innerWidth > 745 ? 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS' : 'COMPET\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0ITIONS'}</h1>
-      </div>
-      <Spline className='splines' scene="https://prod.spline.design/5d1cMbEhisQ1tPpS/scene.splinecode" />
-    </div>
       <div className="competition-container">
-
         {competitionsData.map((competition, index) => (
           <Card
             key={index}
@@ -74,13 +105,11 @@ const Competitions = () => {
             description={competition.description}
             time={competition.time}
             venue={competition.venue}
+            register={competition.register}
           />
         ))}
       </div>
     </div>
-
-
-
   );
 };
 
