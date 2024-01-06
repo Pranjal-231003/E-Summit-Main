c1a31fc24e3b1cbd56fda19fd24ab34bd2fff576
Ayush Bansal <bansalayush24704@gmail.com>
2024-01-06 17:00:09 +0530

    sponsor responsive

--- frontend/src/components/Sponsors/Sponsors.jsx
+++ frontend/src/components/Sponsors/Sponsors.jsx
@@ -6,9 +6,7 @@ const Sponsors = () => {
   return (
     <div className='main'>
         <div className='splinemodel'>
-          <div style={{width: 100+"vw", height:100+"vh", marginLeft: -40 + "vw"}}>
             <Spline scene="https://prod.spline.design/nnzeUzYoO22aP0An/scene.splinecode" />
-          </div>
           {/* https://prod.spline.design/nnzeUzYoO22aP0An/scene.splinecode */}
         </div>
     </div>
