3bdda5620058431f3b91a5498899d9634dd5e465
Pranjal-2310 <pranjalshp2310@gmail.com>
2024-01-22 23:43:22 +0530

    back to normal

--- frontend/src/components/Preloader/Preloader.jsx
+++ frontend/src/components/Preloader/Preloader.jsx
@@ -21,7 +21,8 @@ function Preloader() {
   return (
     <div className="page">
       <video ref={videoRef} width="100%" height="100%" autoPlay muted>
-        <source src={preloaderVideo} type="video/mp4" />
+        <source src='https://res.cloudinary.com/dempuhcss/video/upload/f_auto:video,q_auto/hg3lgbqhfdbmx2ng1sic' type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     
     </div>
